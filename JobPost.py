cd3b642ddb89d977248158a5b728c6e572c9f626
Matthew Grixti <matthewpcgrixti@gmail.com>
2015-10-17 22:14:37 -0600

    JobPost class. Constructor, and getter for userid, description and skills.

---
+++ JobPost.py
@@ -0,0 +1,22 @@
+__author__ = 'Matthew'
+
+class JobPost:
+
+    postID = None;
+    description = None;
+    skills = [];
+
+    # Constructor
+    def __init__(self, user_id, description, skills):
+        JobPost.postID = user_id;
+        JobPost.description = description;
+        JobPost.skills = skills;
+
+    def get_postid(self):
+        return JobPost.postID;
+
+    def get_description(self):
+        return JobPost.description;
+
+    def get_skills(self):
+        return JobPost.skills;
