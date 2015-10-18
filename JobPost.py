4a2cb4b93f6ac55b4092fd42ae23fbf4ff6d5985
Matthew Grixti <matthewpcgrixti@gmail.com>
2015-10-18 11:05:42 -0600

    Fixed sintax.

--- JobPost.py
+++ JobPost.py
@@ -2,21 +2,21 @@ __author__ = 'Matthew'
 
 class JobPost:
 
-    postID = None;
-    description = None;
-    skills = [];
+    postID = None
+    description = None
+    skills = []
 
     # Constructor
     def __init__(self, user_id, description, skills):
-        JobPost.postID = user_id;
-        JobPost.description = description;
-        JobPost.skills = skills;
+        JobPost.postID = user_id
+        JobPost.description = description
+        JobPost.skills = skills
 
     def get_postid(self):
-        return JobPost.postID;
+        return JobPost.postID
 
     def get_description(self):
-        return JobPost.description;
+        return JobPost.description
 
     def get_skills(self):
-        return JobPost.skills;
+        return JobPost.skills
--- User.py
+++ User.py
@@ -2,16 +2,16 @@ __author__ = 'Matthew'
 
 class User:
 
-    userID = None;
-    skills = [];
+    userID = None
+    skills = []
 
     # Constructor
     def __init__(self, user_id, skills):
-        User.userID = user_id;
-        User.skills = skills;
+        User.userID = user_id
+        User.skills = skills
 
     def get_userid(self):
-        return User.userID;
+        return User.userID
 
     def get_skills(self):
-        return User.skills;
+        return User.skills
