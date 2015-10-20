9cd6cc6a7c7a854d14d4935fbb2699ce7dcb13b5
mgrixti <matthewpcgrixti@gmail.com>
2015-10-20 12:59:02 -0600

    added: - Getter for first_name, last_name - Add skill first name     - Appends to array list

--- User.py
+++ User.py
@@ -3,15 +3,34 @@ __author__ = 'Matthew'
 class User:
 
     userID = None
+    first_name = None
+    last_name = None
     skills = []
 
     # Constructor
-    def __init__(self, user_id, skills):
+    # Assigns instant variables their user specific values
+    def __init__(self, user_id, first_name, last_name,skills):
         User.userID = user_id
+        User.first_name = first_name
+        User.last_name = last_name
         User.skills = skills
 
+    # Returns userID of user
     def get_userid(self):
         return User.userID
 
+    # Returns first name of user
+    def get_first_name(self):
+        return User.first_name
+
+    # Returns last name of user
+    def get_last_name(self):
+        return User.last_name
+
+    # Returns array of users skills
     def get_skills(self):
         return User.skills
+
+    # Adds new skill to end of list
+    def add_skill(self, skill):
+        User.skills.append(skill)
