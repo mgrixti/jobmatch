cce99796c7bf04a705f0e66b7ed8acad495c219d
Matthew Grixti <matthewpcgrixti@gmail.com>
2015-11-01 14:30:22 -0700

    Created SQL connect and close functions

--- src/DataAccess/SqlConnect.py
+++ src/DataAccess/SqlConnect.py
@@ -1,4 +1,26 @@
 __author__ = 'mgrixti'
 
-import mysql.connector;
+import mysql.connector
+from mysql.connector import errorcode
 
+class SqlConnect():
+
+    conn = None
+
+    # Establish connection to Database
+    def connect(self):
+        try:
+            SqlConnect.conn = mysql.connector.connect(user='fakeuser', password='notrealpass', host='fakehost',
+                                    database='fakedatabase')
+            print("Connection Successful")
+        except mysql.connector.Error as err:
+            if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+                print("Something is wrong with your user name or password")
+            elif err.errno == errorcode.ER_BAD_DB_ERROR:
+                print("Database does not exist")
+            else:
+                print(err)
+
+    # Close connection to database
+    def close(self):
+        SqlConnect.conn.close()
