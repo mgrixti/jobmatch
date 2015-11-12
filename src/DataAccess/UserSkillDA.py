137e55bec754b2a075a6bb75c3f3627bdd9ab986
Matthew Grixti <matthewpcgrixti@gmail.com>
2015-11-12 14:51:54 -0700

    Created UserSkillDA class with methods: GetAll() GetByID() GetAllForUser()

---
+++ src/DataAccess/UserSkillDA.py
@@ -0,0 +1,50 @@
+__author__ = 'Matthew Grixti'
+
+from sqlalchemy import Column, Integer, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, backref
+from src.DataAccess.DbConnection import DbConnection
+from src.DataAccess.UserDA import UserDA
+from src.DataAccess.SkillDA import SkillDA
+
+
+Base = declarative_base()
+
+class UserSkillDA(Base):
+    __tablename__ = 'user_skill'
+
+    user_skill_id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey(UserDA.user_id))
+    skill_id = Column(Integer, ForeignKey(SkillDA.skill_id))
+
+    user = relationship(UserDA)
+    skills = relationship(SkillDA)
+
+
+    def __init__(self):
+        UserSkillDA.db = DbConnection()
+        UserSkillDA.session = UserSkillDA.db.connect()
+
+    def GetAll(self):
+
+        # SELECT * FROM user_skill
+        results = UserSkillDA.session.query(UserSkillDA)
+        return results
+
+    def GetByID(self):
+
+        results = UserSkillDA.session.query(UserSkillDA).filter(UserSkillDA.user_skill_id == id).one()
+        return results
+
+    def GetAllForUser(self, user_id):
+
+        results = UserSkillDA.session.query(UserSkillDA).join(UserDA).join(SkillDA).\
+                  filter(UserSkillDA.user_id == user_id).all()
+
+        return results
+
+
+#list = UserSkillDA()
+#skills = list.GetAllForUser(3)
+#for skill in skills:
+ #   print(skill.user.user_id, skill.user.first_name.decode(), skill.skills.skill_name.decode())
