2069f9b8572e9a5cae5def71975ca4294f255bb7
Matthew Grixti <matthewpcgrixti@gmail.com>
2015-11-11 18:26:41 -0700

    Created SQL connect function

--- src/DataAccess/SqlConnect.py
+++ src/DataAccess/SqlConnect.py
@@ -1,26 +1,14 @@
-__author__ = 'mgrixti'
+__author__ = 'Matthew Grixti'
 
-import mysql.connector
-from mysql.connector import errorcode
+from sqlalchemy import *
+from sqlalchemy.orm import *
 
 class SqlConnect():
 
-    conn = None
-
     # Establish connection to Database
     def connect(self):
-        try:
-            SqlConnect.conn = mysql.connector.connect(user='fakeuser', password='notrealpass', host='fakehost',
-                                    database='fakedatabase')
-            print("Connection Successful")
-        except mysql.connector.Error as err:
-            if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-                print("Something is wrong with your user name or password")
-            elif err.errno == errorcode.ER_BAD_DB_ERROR:
-                print("Database does not exist")
-            else:
-                print(err)
+        #Create db engine. Pass in connection string.
+        engine = create_engine('mysql+mysqlconnector://jobmatch:Pennyworth2@mysql.mattgrixti.com/jobmatch', echo=True)
 
-    # Close connection to database
-    def close(self):
-        SqlConnect.conn.close()
+        Session = sessionmaker(bind=engine)
+        return Session()
