50d4b823ac35c8ad17dfd0cfbf10db8d41d9f804
Matthew Grixti <matthewpcgrixti@gmail.com>
2015-11-13 20:22:19 -0700

    Created UserControler class with methods BuildSingleUser() and BuildUserObject()

--- src/DataAccess/DbConnection.py
+++ src/DataAccess/DbConnection.py
@@ -8,7 +8,7 @@ class DbConnection():
     # Establish connection to Database
     def connect(self):
         #Create db engine. Pass in connection string.
-        engine = create_engine('mysql+mysqlconnector://jobmatch:Pennyworth2@mysql.mattgrixti.com/jobmatch', echo=True)
+        engine = create_engine('mysql+mysqlconnector://jobmatch:Pennyworth2@mysql.mattgrixti.com/jobmatch', echo=False)
         Session = sessionmaker(bind=engine)
         return Session()
 
