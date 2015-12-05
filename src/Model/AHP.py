a62923ca48ce39c1b6d01ea40fdca52b38d0fa07
Matthew Grixti <Matthewpcgrixti@gmail.com>
2015-12-04 22:25:40 -0700

    AHP row counter

--- src/Model/AHP.py
+++ src/Model/AHP.py
@@ -1,28 +1,36 @@
 
-
+# Holds all AHP related function
 class AHP:
 
     numCriteria = 5
 
+    # Builds matrix of weights
+    # data should be in an array in order of row
     def GenerateMatrix(self, data=[1,1,1,1,1]):
 
         # initialize Matrix to all 1s
         matrix = [[1 for x in range(AHP.numCriteria)] for x in range(AHP.numCriteria)]
         #TODO: code
 
-        for y in range(AHP.numCriteria):
-            for x in range(AHP.numCriteria):
+        # For each row inset/generate opposite row data
+        # todo Fix x advancement
+        row = 0
+
+        for y in range(AHP.numCriteria -1):
+            x = row
+            for x in range(AHP.numCriteria -1):
+
                 if x == y:
                     matrix[x][y] = 1
                 else:
-                    data.pop(0)
+                    #Pop next data off stack
+                    weight = data.pop(0)
+                    matrix[x][y] = weight
+                    matrix[y][x] = AHP.CalculateOppositeWeight(self, weight)
+            row=+ 1
 
 
-
-
-
-
-    def CalculateOppositeWeight(self, weight):
+    def CalculateOppositeWeight(self, weight=1):
 
         # 1 divided by weight to find the opposite corresponding weight for the matrix
         oppositeWeight = 1/weight;
